2d4b83cc32a89a6aa5324cf921e2d30c0c05e84c
markhorodetskyi <sim7rok@gmail.com>
2024-02-26 10:36:41 +0200

    prepare to prod

---
+++ config/db.py
@@ -0,0 +1,18 @@
+from environs import Env
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env = Env()
+env.read_env()
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASS"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.str("DB_PORT"),
+    }
+}
